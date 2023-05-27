777e420cc215b0ddb0e677c4888680d0cef74ed9
Fadil939 <fadhlurrahman939@gmail.com>
2023-05-27 20:16:24 +0700

    memahami kelas abstract

---
+++ php_Oop/abstract/abstract.php
@@ -0,0 +1,12 @@
+<?php 
+//abstract class
+//sebuah class yang tidak dapat di-instansiasi
+//kelas abstrack ini mendefinisikan interface untuk kelas lain yang menjadi keturunannya 
+//berperan sebagai kerangka dasar untuk keturunannya 
+//minimal memiliki 1 method abstrack
+//digunakan dalam 'warisan' /inheritance untuk 'memaksakan' implementasi  method abstrack sama untuk semua kelas keturunanya 
+
+
+
+
+?>
